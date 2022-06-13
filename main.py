bee33bc2d536078b79a5ea999f9965601062b3b0
György Végh <gyorgy.vegh.job@gmail.com>
2022-06-13 13:14:16 +0200

    All_refact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#It draws the game board with the necessary values.
 def drawboard(board):
-    cannot_draw = False
-    d_board = board   
+    cannot_draw = False     
+    d_board = list(map(list, board))
     try:
         for i in range(3):
             for j in range(3):
@@ -16,8 +17,10 @@ def drawboard(board):
         cannot_draw = True    
     return True if cannot_draw == False else False
 
+# If no empty square the player win.
 def checkWinner(board):
     win = True
+    print(board)
     for i in range(len(board[0])):
         for j in range(len(board[0][0])):
             if board[i][j] == " ":
@@ -25,6 +28,7 @@ def checkWinner(board):
                 break
     return win
 
+# It clear the empty squares where no bomb in the neighbourhood.
 def clearNeighbours(board):
     for i in range(9):
         for i in range(3):
@@ -47,55 +51,57 @@ def clearNeighbours(board):
                     board[i-1][j-1] = "_"
     return board
 
+# It shows information about the number of the bombs in the neighbourhood.
 def checkBombs(board):
     for i in range(3):
         for j in range(3):
             c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = False
             if board[i][j] != "B" and board[i][j] != "X" and board[i][j] != "F" and board[i][j] != "FB" and board[i][j] != " ":
+                board[i][j] = "_"
                 for c in range(9):
-                    if i<2 and board[i+1][j] == "B" and c1 == False:
+                    if i<2 and board[i+1][j] in ["B", "FB"] and c1 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1)
                         c1 = True                       
-                    if j<2 and i<2 and board[i+1][j+1] == "B" and c2 == False:
+                    if j<2 and i<2 and board[i+1][j+1] in ["B", "FB"] and c2 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1) 
                         c2 = True
-                    if j<2 and board[i][j+1] == "B" and c3 == False:
+                    if j<2 and board[i][j+1] in ["B", "FB"] and c3 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1) 
                         c3 = True
-                    if i>0 and board[i-1][j] == "B" and c4 == False:
+                    if i>0 and board[i-1][j] in ["B", "FB"] and c4 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1) 
                         c4 = True
-                    if  j>0 and i>0 and board[i-1][j-1] == "B" and c5 == False:
+                    if  j>0 and i>0 and board[i-1][j-1] in ["B", "FB"] and c5 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1) 
                         c5 = True
-                    if  j>0 and board[i][j-1] == "B" and c6 == False:
+                    if  j>0 and board[i][j-1] in ["B", "FB"] and c6 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1)
                         c6 = True
-                    if j>0 and i<2 and board[i+1][j-1] == "B" and c7 == False:
+                    if j>0 and i<2 and board[i+1][j-1] in ["B", "FB"] and c7 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
                             board[i][j] = str(int(board[i][j]) + 1) 
                         c7 = True
-                    if i>0 and j>0 and board[i-1][j-1] == "B" and c8 == False:
+                    if i>0 and j<2 and board[i-1][j+1] in ["B", "FB"] and c8 == False:
                         if board[i][j] == "_":
                             board[i][j] = "1" 
                         else:
@@ -104,8 +110,8 @@ def checkBombs(board):
     print(board)
     return board
 
+# It can remove the flag too if it is needed.
 def addFlag(board, square):
-    # It can remove the flag too if it is needed.
     if board[square[0]][square[1]] == "B":
         board[square[0]][square[1]] = "FB"
     elif board[square[0]][square[1]] == "FB":
@@ -123,6 +129,7 @@ def doStep(board, step):
         board[step[0]][step[1]] = "_"
     return board
 
+#It is the main function.
 def minesweeper(board, sign, square):
     if sign == "S":
         doStep(board, square)
@@ -148,4 +155,14 @@ def minesweeper(board, sign, square):
 #minesweeper([['B','B','B'], ['B',' ','B'], ['B','B','B']],"S",[0,0])
 #minesweeper([[' ',' ',' '], [' ',' ',' '], [' ',' ','B']],"S",[1,1])
 #minesweeper([[' ',' ',' '], ['B','B','B'], [' ',' ','B']],"S",[2,1])
-minesweeper([[' ',' ',' '], ['B','B','B'], ['_','_','B']],"F",[2,2])
+#minesweeper([[' ',' ',' '], ['B','B','B'], ['_','_','B']],"F",[2,2])
+
+# A simple game:
+gameboard = [[' ','B','B'], ['B','B',' '], ['B',' ',' ']]
+minesweeper(gameboard,"S",[2,2])
+minesweeper(gameboard,"F",[1,1])
+minesweeper(gameboard,"F",[0,1])
+minesweeper(gameboard,"F",[0,2])
+minesweeper(gameboard,"F",[1,0])
+minesweeper(gameboard,"F",[2,0])
+minesweeper(gameboard,"S",[0,0])
